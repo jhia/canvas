eaf164ad58945567593e7e6d3c7527858bd34b52
Jhinel Arcaya <proyectoamicoro@gmail.com>
2015-10-12 12:01:52 -0430

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+var width = screen.width;
+var height = screen.height;
+
+var space = document.getElementById("canvas");
+space.setAttribute("width", width);
+space.setAttribute("height", height);
+
+var ctx = space.getContext("2d");
+
+//Start to code!
